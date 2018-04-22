8ab7be66ade9bd39600ef13e236da1de40e3b0c4
Peter Lyons <pete@peterlyons.com>
2018-04-22 13:38:03 -0600

    Initial project boilerplate and test

---
+++ src/lib.rs
@@ -0,0 +1,12 @@
+use std::path::{Path,PathBuf};
+
+pub fn slug(path: &Path) -> PathBuf {
+    PathBuf::from(&path)
+}
+
+
+#[test]
+fn identity() {
+    let input = PathBuf::from("/");
+    assert!(slug(input.as_path()) == input);
+}
---
+++ src/main.rs
@@ -0,0 +1,8 @@
+extern crate slugger;
+use std::path::Path;
+use slugger::slug;
+
+fn main() {
+    println!("Hello, world!");
+    // println!("slug: {}", slug::slug());
+}
