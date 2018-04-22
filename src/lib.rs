be7b88134d959ce16542a01e3856012721c3c504
Peter Lyons <pete@peterlyons.com>
2018-04-22 13:45:28 -0600

    rustfmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,13 @@
-use std::path::{Path,PathBuf};
+use std::path::{Path, PathBuf};
 
 pub fn slug(path: &Path) -> PathBuf {
     PathBuf::from(&path)
 }
 
-
 #[test]
 fn identity() {
     let input = PathBuf::from("/");
+    assert!(slug(input.as_path()) == input);
+    let input = PathBuf::from("/foo");
     assert!(slug(input.as_path()) == input);
 }
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,11 @@
 extern crate slugger;
-use std::path::Path;
+use std::path::PathBuf;
 use slugger::slug;
 
 fn main() {
     println!("Hello, world!");
-    // println!("slug: {}", slug::slug());
+    println!(
+        "slug: {}",
+        slug(&PathBuf::from("/hey/foo bar")).to_string_lossy()
+    );
 }
