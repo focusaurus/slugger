4a8ab876e8ee57a90acf3afbfd8076cc4de58ae2
Peter Lyons <pete@peterlyons.com>
2018-04-22 13:56:50 -0600

    import slugify crate

--- src/lib.rs
+++ src/lib.rs
@@ -10,4 +10,12 @@ fn identity() {
     assert!(slug(input.as_path()) == input);
     let input = PathBuf::from("/foo");
     assert!(slug(input.as_path()) == input);
+    let input = PathBuf::from("/foo/bar/baz-dash-bux.txt");
+    assert!(slug(input.as_path()) == input);
+}
+
+#[test]
+fn space_in_word() {
+    let input = PathBuf::from("/space here.txt");
+    assert!(slug(input.as_path()) == PathBuf::from("/space-here.txt"));
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
+#[macro_use(slugify)]
+extern crate slugify;
 extern crate slugger;
+
 use std::path::PathBuf;
 use slugger::slug;
+use slugify::slugify;
 
 fn main() {
-    println!("Hello, world!");
+    let hey = "/foo/bar baz/bux .txt ";
+    println!("{}", slugify!("Hello, world!"));
+    println!("{}", slugify!(hey));
     println!(
         "slug: {}",
         slug(&PathBuf::from("/hey/foo bar")).to_string_lossy()
