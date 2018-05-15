bc5256aab2560d1d000bfa20dbc29e2304692fce
Peter Lyons <pete@peterlyons.com>
2018-05-15 11:07:53 -0600

    Return Result from main

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ extern crate slugger;
 use rsfs::{GenFS, Metadata, Permissions};
 use std::{env, io};
 
-fn main_result<
+fn slugger_main<
     P: Permissions,
     M: Metadata<Permissions = P>,
     F: GenFS<Permissions = P, Metadata = M>,
@@ -20,13 +20,10 @@ fn main_result<
     slugger::slugger(fs, &args)
 }
 
-fn main() {
+fn main() -> io::Result<()> {
     let args = vec![env::args().skip(1).collect()];
     let mut fs = rsfs::disk::FS;
-    if let Err(message) = main_result(&mut fs, &args) {
-        eprintln!("{}", message);
-        std::process::exit(10);
-    }
+    slugger_main(&mut fs, &args)
 }
 
 #[cfg(test)]
@@ -37,7 +34,7 @@ mod test {
     fn slugger_zero_args_error() {
         use std::error::Error;
         let mut fs = rsfs::mem::FS::new();
-        match main_result(&mut fs, &vec![]) {
+        match slugger_main(&mut fs, &vec![]) {
             Ok(()) => panic!("should return Err with zero args"),
             Err(io_error) => assert!(io_error.description().starts_with("Usage")),
         }
